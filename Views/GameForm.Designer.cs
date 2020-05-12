86a021b0fa9155fc75b1ee1e7a27790bd65a2a56
StSerana <serana.st19@gmail.com>
2020-05-12 16:57:13 +0500

    add some drawn elements

---
+++ Views/GameForm.Designer.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+
+namespace Survival.Views
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize) (this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 561);
+            this.Name = "GameForm";
+            this.Text = "GameForm";
+            ((System.ComponentModel.ISupportInitialize) (this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
---
+++ Views/GameForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Survival.Views
+{
+    public partial class GameForm : Form
+    {
+        private readonly Game game;
+        public GameForm()
+        {
+            game = new Game();
+            game.Start(5,5);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Controls.Add(DrawMap());
+            ClientSize = new Size((game.MapHeight + 1) * 100, (game.MapWidth + 1) * 100);
+            InitializeComponent();
+        }
+
+        private TableLayoutPanel DrawMap()
+        {
+            var layout = new TableLayoutPanel {ColumnCount = game.MapHeight, RowCount = game.MapWidth, Height = (game.MapHeight + 1) * 100, Width = (game.MapWidth + 1) * 100};
+            for (var x = 0; x < game.MapWidth; x++)
+            {
+                for (var y = 0; y < game.MapHeight; y++)
+                {
+                    var image = ResizeImage(GetImage(game.Map[x, y]), new Size(100,100));
+                    var btn = new Button {Image = image, Height = 100, Width = 100};
+                    var posX = x;
+                    var posY = y;
+                    btn.Click += (sender, args) =>
+                    {
+                        game.Human.X = posX;
+                        game.Human.Y = posY;
+                        game.Act(game.Human);
+                        DrawMap();
+                    };
+                    layout.Controls.Add(btn); 
+                }
+            }
+            return layout;
+        }
+
+        private static Image GetImage(ICell cell)
+        {
+            switch (cell)
+            {
+                case Wall _:
+                    return Resources.Wall;
+                case Bomb _:
+                    return Resources.Bomb;
+                case ColorCell colorCell:
+                    switch (cstate: colorCell.State, color: colorCell.Color)
+                    {
+                        case (State.One, Color.Blue):
+                            return Resources.BlueOne;
+                        case (State.Two, Color.Blue):
+                            return Resources.BlueTwo;
+                        case (State.Three, Color.Blue):
+                            return Resources.BlueThree;
+                        case (State.One, Color.Red):
+                            return Resources.RedOne;
+                        case (State.Two, Color.Red):
+                            return Resources.RedTwo;
+                        case (State.Three, Color.Red):
+                            return Resources.RedThree;
+                    }
+
+                    break;
+            }
+
+            return Resources.Empty;
+        }
+
+        private static Image ResizeImage(Image imgToResize, Size size)
+        {
+            return new Bitmap(imgToResize, size);
+        }
+    }
+}
